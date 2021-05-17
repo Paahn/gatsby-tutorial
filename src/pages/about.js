95637022d8254026162edf68b7b465b469ec12dd
Paahn <paahndev@gmail.com>
2021-05-16 19:20:56 -0700

    made about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const about = () => {
+    return (
+        <main>
+            <title>About Me</title>
+            <h1>About Me</h1>
+            <p>Hello there! I am currently learning Gatsby.js, whattyaknow!?</p>
+        </main>
+    )
+}
+
+export default about

d0737926e47943c2d726b762fbeaafbf6b2d63a1
Paahn <paahndev@gmail.com>
2021-05-17 17:15:18 -0700

    link back home

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'gatsby';
 
 const about = () => {
     return (
         <main>
             <title>About Me</title>
             <h1>About Me</h1>
+            <Link to='/'>Back to Home</Link>
             <p>Hello there! I am currently learning Gatsby.js, whattyaknow!?</p>
         </main>
     )
